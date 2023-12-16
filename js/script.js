b7aeb8c0c947a7eb5aa6d37f290ffa99811b9919
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2023-12-16 19:28:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const body = document.querySelector("body");
+const boxes = document.querySelectorAll(".box");
+const toggle = document.querySelector(".toggle");
+const topText = document.querySelectorAll(".top");
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("light");
+  toggle.classList.toggle("active");
+
+  // Iterate through each box and toggle the "active" class
+  boxes.forEach((box) => {
+    box.classList.toggle("light");
+  });
+
+  topText.forEach((top) => {
+    top.classList.toggle("light");
+  });
+});
+
+
+const prefersLightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+
+  // The user prefers dark mode
+
+  if (prefersLightMode) {
+      body.classList.add("light"); // Add the light mode class to the body
+      toggle.classList.toggle("active"); // add an "active" class to your toggle button
+      localStorage.setItem("mode", "light"); // Optionally, store the mode preference in local storage
+     
+  console.log('User prefers dark mode');
+} else {
+  // The user prefers light mode
+  console.log('User prefers light mode');
+}
+
+
