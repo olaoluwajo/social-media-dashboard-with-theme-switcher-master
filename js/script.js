1e416588738befc729df8a6cecefca2ea0456aaa
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2023-12-17 00:31:43 +0100

    refactored

--- js/script.js
+++ js/script.js
@@ -1,37 +1,111 @@
 const body = document.querySelector("body");
 const boxes = document.querySelectorAll(".box");
 const toggle = document.querySelector(".toggle");
-const topText = document.querySelectorAll(".top");
 
-toggle.addEventListener("click", () => {
-  body.classList.toggle("light");
-  toggle.classList.toggle("active");
+const MODES = {
+  DARK: "dark",
+  LIGHT: "light",
+};
 
-  // Iterate through each box and toggle the "active" class
-  boxes.forEach((box) => {
-    box.classList.toggle("light");
-  });
 
-  topText.forEach((top) => {
-    top.classList.toggle("light");
+document.addEventListener("DOMContentLoaded", function () {
+  // Check the current mode from local storage
+  const currentMode = localStorage.getItem("mode");
+
+  let toggleState = false;
+
+  const setDarkMode = () => {
+    toggle.classList.remove("active");
+    body.classList.remove("light");
+    localStorage.setItem("mode", MODES["DARK"]);
+  };
+
+  const setLightMode = () => {
+    toggle.classList.add("active");
+    body.classList.add("light");
+    localStorage.setItem("mode", MODES.LIGHT);
+  };
+
+  const setModeFromMediaQuery = () => {
+    const prefersLightMode =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: light)").matches;
+    console.log(prefersLightMode);
+    if (
+      (currentMode && currentMode === MODES.DARK) ||
+      (!currentMode && !prefersLightMode)
+    ) {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
+  };
+
+  setModeFromMediaQuery();
+
+  toggle.addEventListener("click", () => {
+    toggleState = !toggleState;
+
+    if (toggleState) {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
   });
 });
 
 
-// const prefersLightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+//////////REFACTORED/////////////////
 
 
-//   // The user prefers dark mode
 
-//   if (prefersLightMode) {
-//       body.classList.add("light"); // Add the light mode class to the body
-//       toggle.classList.toggle("active"); // add an "active" class to your toggle button
-//       localStorage.setItem("mode", "light"); // Optionally, store the mode preference in local storage
-     
-//   console.log('User prefers dark mode');
-// } else {
-//   // The user prefers light mode
-//   console.log('User prefers light mode');
-// }
+
+// const body = document.querySelector("body");
+// const boxes = document.querySelectorAll(".box");
+// const toggle = document.querySelector(".toggle");
+
+// const MODES = {
+//   DARK: "dark",
+//   LIGHT: "light",
+// };
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Check the current mode from local storage
+//   const currentMode = localStorage.getItem("mode");
+//   let toggleState = false;
+
+//   const setMode = (mode, addClass, removeClass) => {
+//     toggle.classList[addClass ? "add" : "remove"]("active");
+//     body.classList[addClass ? "add" : "remove"](mode);
+//     localStorage.setItem("mode", mode); //remember  chosen mode
+//   };
+
+//   const setDarkMode = () => {
+//     setMode(MODES.DARK, false, true);
+//   };
+
+//   const setLightMode = () => {
+//     setMode(MODES.LIGHT, true, false);
+//   };
+
+//   const setModeFromMediaQuery = () => {
+//     const prefersLightMode =
+//       window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches;
+//     if ((currentMode && currentMode === MODES.DARK) || (!currentMode && !prefersLightMode)) {
+//       setDarkMode();
+//     } else {
+//       setLightMode();
+//     }
+//   };
+
+//   setModeFromMediaQuery();
+
+//   toggle.addEventListener("click", () => {
+//     toggleState = !toggleState;
+//     toggleState ? setDarkMode() : setLightMode();
+//   });
+// });
+
+
 
 
