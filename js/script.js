588aa92ccb4df1a06643f5be5a65ebf0f644532d
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2023-12-16 19:45:17 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -18,20 +18,20 @@ toggle.addEventListener("click", () => {
 });
 
 
-const prefersLightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+// const prefersLightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
 
 
-  // The user prefers dark mode
+//   // The user prefers dark mode
 
-  if (prefersLightMode) {
-      body.classList.add("light"); // Add the light mode class to the body
-      toggle.classList.toggle("active"); // add an "active" class to your toggle button
-      localStorage.setItem("mode", "light"); // Optionally, store the mode preference in local storage
+//   if (prefersLightMode) {
+//       body.classList.add("light"); // Add the light mode class to the body
+//       toggle.classList.toggle("active"); // add an "active" class to your toggle button
+//       localStorage.setItem("mode", "light"); // Optionally, store the mode preference in local storage
      
-  console.log('User prefers dark mode');
-} else {
-  // The user prefers light mode
-  console.log('User prefers light mode');
-}
+//   console.log('User prefers dark mode');
+// } else {
+//   // The user prefers light mode
+//   console.log('User prefers light mode');
+// }
 
 
